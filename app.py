77ccb5cf3dda8c8e4a117554b5016bdc4d05addc
madison-abshire <mha6@uw.edu>
2026-03-02 10:13:24 -0800

    Merge pull request #5
    * update data with headers
    
    * Merge remote-tracking branch 'origin/main' into madison
    
    * added weather conditions by category plot
    
    * Merge remote-tracking branch 'origin/main' into madison
    
    * added map chart and started the layout
    
    * update python version as per recommendation
    
    * change map color theme
    
    * Merge remote-tracking branch 'origin/main' into madison
    
    * layout changes
    
    * Merge remote-tracking branch 'origin/main' into madison

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from src.filters import date_slider, weather_condition_select
 from src.map_plot import wildfire_worldmap_plot
 from src.bar_chart_occurrence import top_countries_burned_area
 
+
 def main() -> None:
     st.set_page_config(
         page_title="Global Wildfire Occurrences (1881-2025)",
@@ -16,11 +17,12 @@ def main() -> None:
 
     df = load_data("data/Forest_Fires_Dataset_Final.csv")
 
-    with st.container():
+    t1, t2, t3 = st.tabs(["World Map", "Explore by Region", "Explore by Cause"])
+    with t1:
         map_year = date_slider(df, key="map_year")
 
         wildfire_worldmap_plot(df, map_year)
-    with st.container():
+    with t2:
         st.subheader("Top Countries by Total Burned Area")
 
         top_n = st.slider(
@@ -37,7 +39,8 @@ def main() -> None:
 
         with st.expander("Show summary table"):
             st.dataframe(summary, use_container_width=True)
-    with st.container():
+    with t3:
+
         scatter_year = date_slider(df, key="scatter_year")
         selected_condition = weather_condition_select()
 

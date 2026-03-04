d918d7fba5b6f0c4dbafeb88c39a2287f00e54b5
madison-abshire <mha6@uw.edu>
2026-03-03 17:47:27 -0800

    Madison (#9)
    * update data with headers
    
    * added weather conditions by category plot
    
    * added map chart and started the layout
    
    * update python version as per recommendation
    
    * change map color theme
    
    * layout changes
    
    * update scatter formatting
    
    * added color map to improve stability

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ from src.burned_chart import top_countries_burned_area, burned_area_by_region
 from src.filters_burned import burned_area_controls
 from src.cause_bar import cause_country_plot
 
-
 def main() -> None:
     st.set_page_config(
         page_title="Global Wildfire Occurrences (1881-2025)",
@@ -20,7 +19,6 @@ def main() -> None:
     df = load_data("data/Forest_Fires_Dataset_Final.csv")
 
     t1, t2, t3 = st.tabs(["World Map", "Explore by Region", "Explore by Cause"])
-
     with t1:
         map_year = date_slider(df, key="map_year")
         wildfire_worldmap_plot(df, map_year)
@@ -51,15 +49,16 @@ def main() -> None:
             st.dataframe(top_countries_df, use_container_width=True)
             st.write(f"Top {top_k} Regions in {selected_country}")
             st.dataframe(region_df, use_container_width=True)
-
     with t3:
-        scatter_year = date_slider(df, key="scatter_year")
-        selected_condition = weather_condition_select()
-
-        scatter_weather_conditions_plot(df, y_axis_column=selected_condition, year_filter=scatter_year)
-
-        selected_countries = country_select(df, year_filter=scatter_year, key="cause_country_filter")
-        cause_country_plot(df, year_filter=scatter_year, selected_countries=selected_countries)
+        with st.container():
+            country_cause_year = date_slider(df, key="country_cause_year")
+            selected_countries = country_select(df, year_filter=country_cause_year, key="cause_country_filter")
+            cause_country_plot(df, year_filter=country_cause_year, selected_countries=selected_countries)
+        with st.container():
+            st.subheader("Distribution of Cause by Weather Conditions")
+            scatter_year = date_slider(df, key="scatter_year")
+            selected_condition = weather_condition_select()
+            scatter_weather_conditions_plot(df, y_axis_column=selected_condition, year_filter=scatter_year)
 
 
 if __name__ == "__main__":
--- src/scatter_plot.py
+++ src/scatter_plot.py
@@ -1,20 +1,29 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+from src.cause_color_map_util import cause_color_map
 
 #TODO: Rename axes,
-def scatter_weather_conditions_plot(df: pd.DataFrame, y_axis_column: dict, year_filter: str) -> None:
+def scatter_weather_conditions_plot(df: pd.DataFrame, y_axis_column: str, year_filter: str) -> None:
     filtered = df[df['Year'] == year_filter]
+    name_map = {"Humidity_Percent": "Humidity Percent",
+                "Temperature_C": "Temperature (C)",
+                "Wind_Speed_kmh": "Wind Speed (km/h)"}
+    palette = cause_color_map()
 
     fig = px.scatter(filtered,
                      x="Cause",
                      y=y_axis_column,
+                     labels={"Cause": "Cause",
+                             y_axis_column: name_map[y_axis_column]},
                      color="Cause",
                      size="Burned_Area_Km",
                      hover_data=["Country","Region","Year"],
-                     color_discrete_sequence=px.colors.qualitative.Dark2)
+                     color_discrete_map=palette)
+
 
     fig.update_xaxes(categoryorder='category ascending')
 
-    st.plotly_chart(fig, width="content")
+
+    st.plotly_chart(fig, width="stretch")
 

55cc9f653e8196436efebef3a0dcdad301407e47
annaycchi <ycchi@uw.edu>
2026-03-10 15:50:21 -0700

    Update map_plot.py

--- src/map_plot.py
+++ src/map_plot.py
@@ -2,18 +2,21 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-def wildfire_worldmap_plot(df: pd.DataFrame, year_filter) -> None:
-    filtered = df[df['Year'] == year_filter]
+def wildfire_worldmap_plot(df: pd.DataFrame, year_range) -> None:
+    start_year, end_year = year_range
+
+    filtered = df[(df["Year"] >= start_year) & (df["Year"] <= end_year)]
+
     fig = px.scatter_map(filtered,
                          lat='Latitude',
                          lon='Longitude',
                          color="Fires_Count",
                          size="Burned_Area_Km",
-                         title="Historical Wildfire World Map",
+                         title=f"Historical Wildfire World Map ({start_year}-{end_year})",
                          color_continuous_scale="Sunsetdark",
                          hover_data=["Year", "Country", "Region"],
                          width=800,
                          height=700,
                          zoom=1)
 
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, width="stretch")

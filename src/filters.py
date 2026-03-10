f153fea9e4c7777d7e2c067d746b59644104288e
annaycchi <ycchi@uw.edu>
2026-03-10 15:49:28 -0700

    Update filters.py
    unified date slider

--- src/filters.py
+++ src/filters.py
@@ -1,23 +1,29 @@
 import pandas as pd
 import streamlit as st
 
-def date_slider(df: pd.DataFrame, key: str) -> str:
+def date_slider(df: pd.DataFrame) -> tuple:
     # Key variable differentiates the multiple year slider widgets
-    min_date = df["Year"].min()
-    max_date = df["Year"].max()
-    return st.slider("Select date or date-range",
-                           min_value=min_date,
-                           max_value=max_date,
-                            key=key)
+    min_year = int(df["Year"].min())
+    max_year = int(df["Year"].max())
+
+    year_range = st.slider("Select year range",
+                           min_value=min_year,
+                           max_value=max_year,
+                           value=(min_year, max_year))
+    return year_range
 
 def weather_condition_select() -> str:
     cols = ["Temperature_C","Humidity_Percent","Wind_Speed_kmh"]
     return st.selectbox("Select weather condition", cols)
 
 
-def country_select(df: pd.DataFrame, year_filter: str, key: str) -> list:
-    year_filter = int(year_filter)
-    filtered_df = df[df["Year"] == year_filter]
+def country_select(df: pd.DataFrame, year_range: tuple, key: str) -> list:
+    start_year, end_year = year_range
+
+    filtered_df = df[
+        (df["Year"] >= start_year) &
+        (df["Year"] <= end_year)
+        ]
 
     countries = sorted(filtered_df["Country"].unique())
 
